0499e7316c74fb31fa47083881528940da9890bf
Giliardi Rigotti <giliardi.oliveira@itajai.sc.gov.br>
2023-02-17 10:50:10 -0300

    Initial Commit

---
+++ Memoria/MemoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriaApp.swift
+//  Memoria
+//
+//  Created by SETEC on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
