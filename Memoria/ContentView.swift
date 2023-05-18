090a836c1c2f14688f3f8ed481ef610e63cc7688
Giliardi Rigotti <giliardi.oliveira@itajai.sc.gov.br>
2023-05-18 11:13:14 -0300

    first commit

--- Memoria/ContentView.swift
+++ Memoria/ContentView.swift
@@ -9,13 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+            .padding(.horizontal)
+            .foregroundColor(.red)
+       
     }
 }
+
+struct CardView: View{
+    @State var isFaceUp: Bool = false
+    var body: some View {
+        ZStack{
+            let shape: RoundedRectangle = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp{
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text("👌").font(.largeTitle)
+            }else{
+                shape.fill()
+            }
+        }
+        .onTapGesture{
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
